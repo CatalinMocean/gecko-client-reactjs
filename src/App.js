bb0841c6199282621b862779330fbd7f04a0807d
Catalin Mocean <moceancatalin14@yahoo.com>
2021-10-27 18:03:08 +0300

    install fontawsome, add description and links in details page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import Button from 'react-bootstrap/Button';
-import './App.css';
+import React, { useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './pages/home/home';
+import Details from './pages/details/details';
+import FourZeroFour from './pages/fourZeroFour/fourZeroFour';
+import Header from './components/header/header';
 
 function App() {
+
   return (
     <div className="App">
-      <Button>Click Me</Button>
-    </div>
+      <Header />
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/details/:id'>
+            <Details />
+          </Route>
+          <Route>
+            <FourZeroFour/>
+          </Route>
+        </Switch>
+      </Router >
+    </div >
   );
 }
 
